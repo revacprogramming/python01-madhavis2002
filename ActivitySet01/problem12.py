d21dbf786b4dfd1df032ed28aecf8fb1ab1ac205
Madhavi S <97089896+madhavis2002@users.noreply.github.com>
2022-07-17 12:15:52 +0000

    program 12

--- ActivitySet01/problem12.py
+++ ActivitySet01/problem12.py
@@ -1,2 +1,12 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/regex
+# Dictionaries
+
+filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+text = open(name)
+max=dict()
+for line in text :
+  line.rstrip()
+  if not line.startswith ("from"):
+    continue :
+    word=line.split()
